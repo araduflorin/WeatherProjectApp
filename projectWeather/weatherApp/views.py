9bedb9959bd4e6a42fa1ce4254cf3e22429a6fe7
araduflorin <araduflorin@yahoo.com>
2024-01-08 22:22:18 +0200

    'ver2'

--- projectWeather/weatherApp/views.py
+++ projectWeather/weatherApp/views.py
@@ -1,14 +1,34 @@
 import requests
 from django.shortcuts import render
 
-from projectWeather.projectWeather.secret_key import value_secret_key
-
+from projectWeather.secret_key import value_secret_key
+from .models import City
+from .forms import CityForm
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=' + value_secret_key
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + value_secret_key
 
-    city = 'Barcelona'
+    # city = 'Barcelona'
+    cities = City.objects.all()  # return all the cities in the database
 
-    city_weather = requests.get(url.format(city)).json()  # request the API data and convert the JSON to Python data types
-    print(city_weather)
-    return render(request, 'weatherApp/index.html')  # returns the index.html template
+    if request.method == 'POST':  # only true if form is submitted
+        form = CityForm(request.POST)  # add actual request data to form for processing
+        form.save()  # will validate and save if validate
+
+    form = CityForm()
+
+    weather_data = []
+
+    for city in cities:
+        city_weather = requests.get(url.format(city)).json()  # request the API data and convert the JSON to Python data types
+    # print(city_weather)
+        weather = {
+            'city': city,
+            'temperature': city_weather['main']['temp'],
+            'description': city_weather['weather'][0]['description'],
+            'icon': city_weather['weather'][0]['icon']
+        }
+        weather_data.append(weather)  # add the data for the current city into our list
+
+    context = {'weather_data': weather_data, 'form' : form}
+    return render(request, 'weatherApp/index.html',context)  # returns the index.html template
